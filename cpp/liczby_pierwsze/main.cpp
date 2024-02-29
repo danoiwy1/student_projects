fef80a03c395934e653146bc0bba799b65b194d7
danoiwy1 <dmatuszynski_20i3@e.zseeim.edu.pl>
2024-02-29 09:24:02 +0000

    studentmachine send

--- cpp/liczby_pierwsze/main.cpp
+++ cpp/liczby_pierwsze/main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
-
+#include <time.h>
 using namespace std;
 
 int main(){
-	int liczby_pierwsze[1000];
-	for(int i=2;i<1001;i++){
-	for(int x=0;x<1000;x++){
-
-	liczby_pierwsze[x]=i;
-    }};
-	for(int y=0;y<1000;y++){
-	cout<<liczby_pierwsze[y]<<",";
-};
+	int 
 	return 0;
 }
